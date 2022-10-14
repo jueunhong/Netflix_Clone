99971a6e0d3af8972bcb70a797ebdf54d9e7fd57
jueunhong <85168145+jueunhong@users.noreply.github.com>
2022-10-14 22:54:21 +0900

    홈 화면에 원형 이미지, 사각형 이미지 슬라이더 위젯 추가하기

--- netflix/lib/screen/home_screen.dart
+++ netflix/lib/screen/home_screen.dart
@@ -1,8 +1,8 @@
-import 'dart:html';
-
 import 'package:flutter/material.dart';
 import 'package:netflix/model/model_movie.dart';
+import 'package:netflix/widget/box_slider.dart';
 import 'package:netflix/widget/carousel_silder.dart';
+import 'package:netflix/widget/circle_slider.dart';
 
 class HomeScreen extends StatefulWidget {
   _HomeScreenState createState() => _HomeScreenState();
@@ -10,6 +10,18 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   List<Movie> movies = [
+    Movie.fromMap({
+      'title': '사랑의 불시착',
+      'keyword': '사랑/로맨스/판타지',
+      'poster': 'test_movie_1.png',
+      'like': false
+    }),
+    Movie.fromMap({
+      'title': '사랑의 불시착',
+      'keyword': '사랑/로맨스/판타지',
+      'poster': 'test_movie_1.png',
+      'like': false
+    }),
     Movie.fromMap({
       'title': '사랑의 불시착',
       'keyword': '사랑/로맨스/판타지',
@@ -49,7 +61,11 @@ class _HomeScreenState extends State<HomeScreen> {
             CarouselImage(movies: movies),
             TopBar(),
           ],
-        )
+        ),
+        CircleSlider(movies: movies),
+        BoxSlider(
+          movies: movies,
+        ),
       ],
     );
   }
